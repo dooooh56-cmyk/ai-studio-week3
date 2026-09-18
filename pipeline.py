21876570c5119f5222189b27e8ccf58f9bd61eaa
Kim Dohyeok <dooooh56@gmail.com>
2026-09-18 13:44:03 +0900

    feat: groupby aggregate

--- pipeline.py
+++ pipeline.py
@@ -8,10 +8,22 @@ BASE_DIR = Path(__file__).resolve().parent
 file_path = BASE_DIR / "RAW_DATA.csv"
 df = pd.read_csv(file_path, encoding='EUC-KR')
 
-#print(df.shape)
-#print(df.info())
+print(df.shape)
+print(df.info())
 
 # 2. 정제/파생 열 생성
 df["단가"] = (pd.to_numeric(df["단가"].astype(str).str.replace(",", "", regex=False), errors="coerce").astype("int64"))
 df["매출액"] = df["단가"] * df["수량"]
 print(df.head())
+
+# 3. '월'컬럼 추가, groupby 집계
+df["주문일자"] = pd.to_datetime(df["주문일자"])
+df["월"] = df["주문일자"].dt.month
+print(df.head())
+print(df.tail())
+
+report = df.groupby(["월", "카테고리"])["매출액"].agg(
+  총매출="sum", 평균매출="mean", 거래건수="count"
+)
+report = report.reset_index()
+print(report)
